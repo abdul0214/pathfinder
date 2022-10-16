8b9b8c11ce0dd37ff802c658663a816034894f5c
Abdul <abdul.wahab@monese.com>
2022-10-16 13:29:28 +0300

    finalising

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ A simple take home assignment from Camunda based on their own BPMN interface
   - run the jar file `java -jar pathfinder-0.0.1-SNAPSHOT-all.jar approveInvoice invoiceProcessed` 
 - To run as a gradle application:
   - `gradle bootRun --args="approveInvoice invoiceProcessed" -q`
+
+## Additional Questions
+
+ - How long did it take you to solve the exercise?
+   - It took me 3-4 hours to do this assignment
+  - What are some edge cases that you might not have considered yet?
+    - There may be multiple routes b/w a source and target node that I have not covered.
+  - What kind of problems/limitations can you think of in terms of your implementation?
+    -  I have not covered the reverse path. i.e. what if source and target nodes are reversed such as `java -jar pathfinder-0.0.1-SNAPSHOT-all.jar invoiceProcessed approveInvoice`
+    - No support to present & find alternate paths
+    - No support to find/confirm the shortest path 
+
--- src/main/java/PathfinderApplication.java
+++ src/main/java/PathfinderApplication.java
@@ -1,15 +1,18 @@
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 public class PathfinderApplication {
 
-    private static final PathFinderService pathFinderService = new PathFinderService();
+    static final PathFinderService pathFinderService = new PathFinderService();
 
-    private static final String bpmnFileUrl = "https://n35ro2ic4d.execute-api.eu-central-1.amazonaws.com" +
+    static final String bpmnFileUrl = "https://n35ro2ic4d.execute-api.eu-central-1.amazonaws.com" +
             "/prod/engine-rest/process-definition/key/invoice/xml";
 
     public static void main(String[] args) {
         if (args.length != 2){
-            throw new IllegalArgumentException("incorrect input");
+            System.out.println("incorrect input");
+            System.exit(-1);
         }
         else {
             String sourceNodeId = args[0];
@@ -17,11 +20,17 @@ public class PathfinderApplication {
             RestTemplate restTemplate = new RestTemplate();
             try {
                 BpmnFile response = restTemplate.getForObject(bpmnFileUrl, BpmnFile.class);
-                System.out.println(pathFinderService.findPath(sourceNodeId, targetNodeId, response.getBpmn20Xml()));
+                List<String> path = pathFinderService.findPath(sourceNodeId, targetNodeId, response.getBpmn20Xml());
+                if (path == null)
+                {
+                    System.exit(-1);
+                }
+                System.out.println("The path from approveInvoice to invoiceProcessed is:" + path);
+                System.exit(0);
             } catch (Exception clientErrorException) {
-                System.out.println("error in fetching file: " + clientErrorException.getMessage());
+                System.out.println("error in fetching bpmn file: " + clientErrorException.getMessage());
+                System.exit(-1);
             }
-            System.exit(0);
         }
     }
 }
